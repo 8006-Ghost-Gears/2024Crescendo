1cbbea30fa847bb161fecb42f823dcea5c8dc0ec
andrewmolina07 <106282964+yurislavic@users.noreply.github.com>
2024-03-05 13:49:28 -0800

    Update RobotContainer.java

--- YAGSL-Example-main/src/main/java/frc/robot/RobotContainer.java
+++ YAGSL-Example-main/src/main/java/frc/robot/RobotContainer.java
@@ -185,7 +185,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("DriveForwardAuto");
+    return drivebase.getAutonomousCommand(AutoChooser);
   }
 
   public void setDriveMode()
