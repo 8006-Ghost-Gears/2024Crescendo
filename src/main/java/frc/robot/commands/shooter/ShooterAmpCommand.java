303a71f270e4fc2becb5959da98c8f241b883267
Spooky <mola9074@chsphantoms.org>
2024-04-01 11:07:26 -0700

    Update ShooterAmpCommand.java

--- src/main/java/frc/robot/commands/shooter/ShooterAmpCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShooterAmpCommand.java
@@ -25,7 +25,10 @@ public class ShooterAmpCommand extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) 
+  {
+    RobotContainer.m_ShooterSubsystem.stopShooter();
+  }
 
   // Returns true when the command should end.
   @Override
